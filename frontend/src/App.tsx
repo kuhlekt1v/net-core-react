739c159290f7b73201efa0f2316a6a975286cfcb
Cody Sheridan <sheridan.codyj@gmail.com>
2021-07-23 14:36:47 -0400

    Initial commit.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { fontFamily, fontSize, gray2 } from './Styles';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+
+function App() {
+  return (
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
